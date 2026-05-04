71c52527f40b8ef40293f0d060af6d62a55b2d9c
Tate Bosler <tatebosler@gmail.com>
2026-05-04 08:37:15 -0500

    add drivers for Vonage and Postmark

--- resources/views/components/estimates.blade.php
+++ resources/views/components/estimates.blade.php
@@ -10,7 +10,7 @@ new class extends Component
 
 <div class="flex flex-col">
     <div class="max-sm:mt-4 flex max-sm:flex-col items-center sm:gap-4 px-4 sm:px-8">
-        <x-logo horizontal class="h-24" />
+        <a href="{{ route('home') }}"><x-logo horizontal class="h-24" /></a>
         <h1 class="ml-auto max-sm:mr-auto pb-2 sm:pt-2">Entry Time Estimates</h1>
     </div>
     <div class="sticky top-0 mb-4 mx-4">
